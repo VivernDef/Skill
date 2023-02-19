acac52714df5b0f680621ad06d18254d0e7c71eb
VivernDef <def_vivern@mail.ru>
2023-02-19 17:43:46 +0300

    Logging

---
+++ D 16.4/NewsPortal/news/app.py
@@ -0,0 +1,8 @@
+
+import redis
+
+red = redis.Redis(
+    host='redis://:redis-19987.c293.eu-central-1-1.ec2.cloud.redislabs.com',
+    port=19987,
+    password='LgdckVgpsWCrRYsuGZAg4R6MiLDYUMkB',
+)
