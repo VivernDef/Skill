ffe811c8d67b3aaa0735b62f26f493b0cd1c7053
VivernDef <def_vivern@mail.ru>
2023-03-27 17:47:49 +0300

    Cleanup

--- D 6.6/NewsPortal/news/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import PostList, PostInDetail
-
-urlpatterns = [
-    path('', PostList.as_view()),
-    path('<int:pk>', PostInDetail.as_view()),
-]
--- D 9.5/NewsPortal/NewsPortal/app.py
+++
@@ -1,8 +0,0 @@
-
-import redis
-
-red = redis.Redis(
-    host='redis-19987.c293.eu-central-1-1.ec2.cloud.redislabs.com',
-    port='19987',
-    password='LgdckVgpsWCrRYsuGZAg4R6MiLDYUMkB',
-)
